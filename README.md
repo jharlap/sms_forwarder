9444fae391ba52619849da59bcbce6ee26b06fc3
Jonathan Harlap <jharlap@gmail.com>
2016-06-07 09:22:35 -0400

    Add basic forwarder/responder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Forwards SMS messages received at a Twilio number to a designated target number.
+
+Set environment variables FORWARD_TO with an E.164 format number to forward the message to, along with PORT for the port to listen on (for Herkou).
+
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var forwardTo string
+
+func main() {
+	port := os.Getenv("PORT")
+	forwardTo = os.Getenv("FORWARD_TO")
+
+	http.ListenAndServe("0.0.0.0:"+port, http.HandlerFunc(handle))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Println("Error parsing form:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	from := r.Form.Get("From")
+	body := r.Form.Get("Body")
+
+	log.Printf("Message from %s: %s", from, body)
+
+	w.Header().Set("Content-Type", "application/xml")
+	tpl := `<?xml version="1.0" encoding="UTF-8" ?>
+<Response>
+    <Message>This number does not support texts. Please call.</Message>
+    <Message to="%s">From %s:
+    %s</Message>
+</Response>
+}
+`
+	fmt.Fprintf(w, tpl, forwardTo, from, body)
+}
